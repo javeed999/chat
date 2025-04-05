aa6b0636fe3e020bfd9dcd145ae55910fb5e4779
shaik-javeed <you@example.com>
2025-04-05 10:57:13 +0530

    first commit

--- fronted/my-react-app/src/components/miscallenous/MyChat.jsx
+++ fronted/my-react-app/src/components/miscallenous/MyChat.jsx
@@ -40,7 +40,7 @@ const MyChat = ({fetchAgain}) => {
   },[fetchAgain])
 
   return (
-    <Box display={{base:selectedChat ?"none":"flex" ,md:"flex"}}flexDirection={"column"} alignItems={"center"} p="3" bg="white" w={{base:"100%",md:"31%"}} borderRadius={"lg"} borderWidth={"1px"} >
+    <Box display={{base:selectedChat ?"none":"flex" ,md:"flex"}} flexDirection={"column"} alignItems={"center"} p="3" bg="white" w={{base:"100%",md:"31%"}} borderRadius={"lg"} borderWidth={"1px"} >
     <Box pb="3" px="3"  fontSize={{base:"28px",md:"30px"}} fontFamily={"Work Sans"}  display="flex" w="100%" flexDirection={"row"}  justifyContent={"space-between"} alignItems="center">
             My Chats
       <GroupChatModel>
